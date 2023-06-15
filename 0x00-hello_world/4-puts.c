ffbc1e62ca30547161479204d3cc554373052d8a
Benedictgithub-Alx <ayuen.2830@gmail.com>
2023-06-15 05:45:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-/** 
- * *main - Entry point
- * * Return: Always 0 (Success)
- *  */
-int main(void)
+#include <stdio.h>
+/**
+ *  * main - A program that prints a line with puts function
+ *   * Return: 0 (Success) 
+ *    */
+int main(void) 
 {
-	ual puzzle").
-		puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	    puts("\"Programming is like building a multilingual puzzle");
+	        return(0);
+} 
