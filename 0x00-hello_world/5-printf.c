e0201d7f21dabacbaf5c5432f5df9f206a843b25
Benedictgithub-Alx <ayuen.2830@gmail.com>
2023-06-15 06:39:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *   * main - A program that print a line using the printf function
- *     *Return: 0 (Success) 
- *     int main(void) 
- *     {
- *       printf("Thou art a masterpiece of creation,\n");
- *         printf("A symphony of colors, shapes, and forms.\n");
- *           printf("Thine every movement is a work of art,\n");
- *             printf("Thy very existence a masterpiece to behold.\n");
- *               return 0;
- *               } 
+ * main-A program that print a line using a printf function
+ * Return:0 (Success)
+ */
+int main(void)
+{
+printf("with proper gramar,but the outcome is a piece of art,\n")
+return(0);
+} 
