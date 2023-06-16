51b3689136da2ccd53870f49ccbd3168c084c6f3
Benedictgithub-Alx <ayuen.2830@gmail.com>
2023-06-16 00:57:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.
- * Return: Always 1.
+ * main - 101-quote.c
+ * description - write a C program that prints exactly and that piece of 
+ * art is useful" - Dora Korpar, 2015-10-19, followed by a new line,
+ * to the standard error.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	      "and that piece is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
